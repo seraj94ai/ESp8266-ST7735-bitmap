84edd7bb6a4ce6fa43b73eb76a9263ce3ce70144
Seraj Alhamidi <42117360+seraj94ai@users.noreply.github.com>
2022-06-14 23:04:45 +0300

    Add files via upload

---
+++ tftIcons.ino
@@ -0,0 +1,36 @@
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library
+
+#include "bitmaps.h"
+#include "bitmapsLarge.h"
+
+// For the breakout, you can use any 2 or 3 pins
+// These pins will also work for the 1.8" TFT shield
+#define TFT_CS     15
+#define TFT_RST    0  // you can also connect this to the Arduino reset
+                      // in which case, set this #define pin to 0!
+#define TFT_DC     2
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+
+void setup() {
+	tft.initR(INITR_BLACKTAB);
+	tft.setRotation(0);
+	tft.fillScreen(ST7735_BLACK);
+
+
+//Case 2: Multi Colored Images/Icons
+  int h = 100,w = 100, row, col, buffidx=0;
+  for (row=0; row<h; row++) { // For each scanline...
+    for (col=0; col<w; col++) { // For each pixel...
+      //To read from Flash Memory, pgm_read_XXX is required.
+      //Since image is stored as uint16_t, pgm_read_word is used as it uses 16bit address
+      tft.drawPixel(col, row, pgm_read_word(evive_in_hand + buffidx));
+      buffidx++;
+    } // end pixel
+  }
+}
+
+void loop() {
+}
